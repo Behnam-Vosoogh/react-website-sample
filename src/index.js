e8967df5787bcf3e376117d86a46baba97c25fbe
Behnam <behnam.vosoogh@outlook.com>
2024-02-23 20:53:02 -0500

    list rendering added

--- src/index.js
+++ src/index.js
@@ -69,7 +69,12 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
+      {/* <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
         photoName="pizzas/spinaci.jpg"
@@ -80,7 +85,7 @@ function Menu() {
         ingredients="Tomato,mushrooms"
         price={12}
         photoName="pizzas/funghi.jpg"
-      />
+      /> */}
       {/* <Pizza />
       <Pizza />
       <Pizza /> */}
@@ -89,14 +94,14 @@ function Menu() {
 }
 function Pizza(props) {
   return (
-    <div>
-      <img src={props.photoName} alt={props.name} />
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price + 3}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price + 3}</span>
       </div>
-    </div>
+    </li>
   );
 }
 function Footer() {
